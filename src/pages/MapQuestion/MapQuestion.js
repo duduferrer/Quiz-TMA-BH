338c1dd2c1db4ed26f568b60f7b00d31cd49935b
Dudu <smingolera@gmail.com>
2023-01-18 03:26:53 -0300

    add endgame

--- src/pages/MapQuestion/MapQuestion.js
+++ src/pages/MapQuestion/MapQuestion.js
@@ -1,11 +1,10 @@
 import { useContext, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 import map from '../../assets/images/13x16 sem legenda AD.png'
 import getAirdromesData from '../../services/retrieveAD';
+import Home from '../Home/Home';
 import '../MapQuestion/MapQuestion.css'
 
-//TODO Prevenir que repita AD
-//TODO Adicionar esquema de pontuação
-//TODO Adicionar fim de jogo
 
 export default function MapQuestion(){
     const answerSize = 30;
@@ -15,6 +14,7 @@ export default function MapQuestion(){
     const [ansColor, setColor] = useState();
     const [nameOrICAO, setNameOrICAO] = useState();
     const [score, setScore] = useState(0);
+    const [isOver, setIsOver] = useState(false);
     const [airdrome, setAirdrome] = useState({
         id: 0,
         name: "",
@@ -26,6 +26,9 @@ export default function MapQuestion(){
     
 
     async function newGame(){
+        setIsOver(false)
+        setScore(0)
+        console.log('clicou')
         localStorage.clear()
         await getADArray()
         newQuestion()
@@ -46,26 +49,41 @@ export default function MapQuestion(){
         
         
     function selectAD(){
-        let airdromesArray = JSON.parse(localStorage.getItem('airdromesArray'))
+        let airdromesArray = (JSON.parse(localStorage.getItem('airdromesArray')))
         let total = airdromesArray.length
-        let i = Math.floor(Math.random()*total)
-        setAirdrome({
-            id: airdromesArray[i].id,
-            name: airdromesArray[i].name,
-            icao: airdromesArray[i].icao,
-            ans_x: airdromesArray[i].ans_x,
-            ans_y: airdromesArray[i].ans_y,
-        })
-        let type = Math.floor(Math.random()*2)
-            switch (type){
-                case 0:
-                    setNameOrICAO(airdromesArray[i].name)
-                    break;
-                case 1:
-                    setNameOrICAO(airdromesArray[i].icao)
-                    break;
-            }
-        
+        if(total == 0){ 
+            endGame()
+        }else{
+            let i = Math.floor(Math.random()*total)
+            setAirdrome({
+                id: airdromesArray[i].id,
+                name: airdromesArray[i].name,
+                icao: airdromesArray[i].icao,
+                ans_x: airdromesArray[i].ans_x,
+                ans_y: airdromesArray[i].ans_y,
+            })
+            let type = Math.floor(Math.random()*2)
+                switch (type){
+                    case 0:
+                        setNameOrICAO(airdromesArray[i].name)
+                        break;
+                    case 1:
+                        setNameOrICAO(airdromesArray[i].icao)
+                        break;
+                }
+        }
+    }
+    function removeAlreadyAns(){
+        localStorage.setItem('correctAnswer', JSON.stringify(airdrome.id))
+        const valueToRemove = localStorage.getItem("correctAnswer")
+        let airdromesArray = (JSON.parse(localStorage.getItem('airdromesArray')))
+        airdromesArray = airdromesArray.filter(x=>'"'+x.id+'"' !== valueToRemove)
+        localStorage.setItem('airdromesArray', JSON.stringify(airdromesArray))
+
+    }
+
+    function endGame(){
+        setIsOver(true);
     }
         
     
@@ -81,26 +99,41 @@ export default function MapQuestion(){
         setWrong(false);
         setColor('green');
         updateScore(50)
-        localStorage.setItem('corretos', airdrome.id)
+        removeAlreadyAns()
     }
     function wrongAns(){
         setWrong(true);
         setCorrect(false);
         setColor('red');
-        updateScore(-20)
+        updateScore(-10)
     }
 
     function updateScore(pts){
-        setScore(score + pts)
+        if(score+pts<0){
+
+        }else{
+            setScore(score + pts)
+        }
     }
 
     
     return(    
         <div className="app">
+            {isOver?
+                <div style={{backgroundColor:'#000000cc', width: '100%', height:'100em', position: 'absolute', zIndex: 5, margin: 0}}>
+                    <div className='screenCover'>
+                        <h2>Fim de Jogo!</h2>
+                        <h3>Você quer voltar para página inicial ou jogar novamente?</h3>
+                        <h3>Pontuação: {score}pts.</h3>
+                        <button onClick={newGame} style={{width: '20em', height:'3em', marginTop: 100}}>Jogar Novamente</button>
+                        <button style={{width: '20em', height:'3em', marginTop: 100}}><Link to={'/'}>Voltar para o Menu</Link></button>
+                    </div>
+                </div>:""
+            }
                 
             {isBlocked?
                 <div style={{backgroundColor:'#000000cc', width: '100%', height:'100em', position: 'absolute', zIndex: 5, margin: 0}}>
-                    <div id='howToPlay'>
+                    <div className='screenCover'>
                         <h2>Como jogar:</h2>
                         <h3>Você será perguntado sobre a localização dos principais aeródromos da terminal BH. Clique onde você acha que está o aeródromo. Caso você acerte, ganhará pontos! Caso erre, você pode tentar de novo!</h3>
                         <button onClick={newGame} style={{width: '20em', height:'3em', marginTop: 100}}>Jogar</button>
