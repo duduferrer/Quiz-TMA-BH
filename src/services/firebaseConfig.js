a84d04cc2b3e2fcabfec1748f94d0133fa0ab236
Dudu <smingolera@gmail.com>
2023-01-18 13:33:35 -0300

    added .env

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD09JcAWyxLth0G7bibV-sV495qljM9e_s",
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: "quizappbh.firebaseapp.com",
   projectId: "quizappbh",
   storageBucket: "quizappbh.appspot.com",
   messagingSenderId: "787319811699",
-  appId: "1:787319811699:web:b96971a73f45164ecb8406",
+  appId: process.env.REACT_APP_APP_ID,
   measurementId: "G-7K2KJW06FC"
 };
 
